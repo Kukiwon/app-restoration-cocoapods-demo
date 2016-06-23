887253d3a174a8d30037c1fad7f3c4aea308fd95
Jordy van Kuijk <info@npsoftware.nl>
2016-06-23 22:22:34 +0200

    initial commit

--- Example/restoration/AppDelegate.swift
+++ Example/restoration/AppDelegate.swift
@@ -12,10 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        window?.makeKeyAndVisible()
+        return true
+    }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        print(NSBundle.mainBundle())
         return true
     }
 
@@ -41,6 +46,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
+    
+    func application(application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
 }
 
--- restoration.podspec
+++ restoration.podspec
@@ -28,13 +28,13 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/<GITHUB_USERNAME>/restoration.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '9.0'
 
   s.source_files = 'restoration/Classes/**/*'
-  
-  # s.resource_bundles = {
-  #   'restoration' => ['restoration/Assets/*.png']
-  # }
+  s.resource = 'restoration/Assets/PodStoryboard.storyboard'
+#s.resource_bundles = {
+#    'Storyboards' => ['restoration/Assets/*.storyboard']
+#  }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
